eaf0e6af4e3210850a94a64d48b15e8424775ed8
Jon Fearer <fearer.jon@gmail.com>
2021-08-05 15:28:34 -0600

    feat(patterns): re-organize by type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 console.log('\x1b[32m', `
 
-  Welcome to TypeScript design patterns!
-  Run 'npm test <pattern>' in order to execute code that demonstrates a particular pattern.
-  For example try: 'npm test strategy'.
+Welcome to TypeScript design patterns!
+Run 'npm test <pattern>' in order to execute code that demonstrates a particular pattern.
+For example try: 'npm test strategy'.
 
 `);
