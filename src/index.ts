ef951348cbab60cc0802edb618a30976050db947
Jon Fearer <fearer.jon@gmail.com>
2021-08-09 11:41:13 -0600

    test(jest): only test .ts files

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ console.log('\x1b[32m', `
 
 Welcome to TypeScript design patterns!
 Run 'npm test <pattern>' in order to execute code that demonstrates a particular pattern.
-For example try: 'npm test strategy'.
+For example try: 'npm test singleton'.
 
 `);
